a7fe12752c651c0009ba01b03a27d282c5d80cd8
suraj111999 <60891874+suraj111999@users.noreply.github.com>
2020-05-26 19:39:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// //for sticky nav-bar
+var nav=document.querySelector(".flex-container");
+var navTop=nav.offsetTop;
+window.addEventListener("scroll",function(){
+  if(window.scrollY >= navTop){
+    document.body.classList.add("scroll");
+
+  }
+  else{
+      document.body.classList.remove("scroll");
+
+  }
+})
+
+
+//for navbar
+var toggle=document.getElementById("nav-toggle");
+var navMenu=document.getElementById("list");
+toggle.onclick=function(){
+   if(navMenu.offsetParent === null){
+     navMenu.style.display="block" ;
+     }
+     else{
+   navMenu.style.display ="none";
+     }
+}
